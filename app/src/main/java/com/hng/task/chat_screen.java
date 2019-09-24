f6e09ba8300b4ee6fd948e10218c3fe1387f86d9
missTinuade <estherjolugba@yahoo.com>
2019-09-24 17:10:52 +0000

    my intitial commit

---
+++ app/src/main/java/com/hng/task/chat_screen.java
@@ -0,0 +1,14 @@
+package com.hng.task;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chat_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_screen);
+    }
+}
