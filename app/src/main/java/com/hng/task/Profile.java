73dceec91a66b8be9b195c5548d74633aab58c7f
MrTech <ogwo.chinaza@gmail.com>
2019-09-29 02:56:06 +0100

    profile image now working

--- app/src/main/java/com/hng/task/Profile.java
+++ app/src/main/java/com/hng/task/Profile.java
@@ -1,28 +1,46 @@
 package com.hng.task;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.hng.task.models.User;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.IOException;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Profile extends AppCompatActivity {
 
     private static final String TAG = "Profile";
+    private static final int PICTURE_RESULT = 42;
 
     //widgets
     private EditText profileName;
@@ -30,6 +48,7 @@ public class Profile extends AppCompatActivity {
     private EditText profileContact;
     private CircleImageView profileImage;
     private TextView logout;
+    private String g;
 
     //firebase variables
     private FirebaseAuth mAuth;
@@ -44,7 +63,7 @@ public class Profile extends AppCompatActivity {
         //init firebase
         mAuth = FirebaseAuth.getInstance();
         mFirebaseDatabase = FirebaseDatabase.getInstance();
-        String g = mAuth.getCurrentUser().getUid();
+        g = mAuth.getCurrentUser().getUid();
         user = mFirebaseDatabase.getReference("users").child(g);
 
         //init widgets
@@ -89,5 +108,75 @@ public class Profile extends AppCompatActivity {
                 }
             }
         });
+
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(intent.createChooser(intent, "Insert Picture"), PICTURE_RESULT);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICTURE_RESULT && resultCode == RESULT_OK) {
+            Uri imageUri = data.getData();
+            final StorageReference ref = FirebaseStorage.getInstance().getReference().child(g + "/image");
+            ref.putFile(imageUri).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(final UploadTask.TaskSnapshot taskSnapshot) {
+                    Task<Uri> firebaseUri = taskSnapshot.getStorage().getDownloadUrl();
+                    firebaseUri.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String url = uri.toString();
+                            String pictureName = taskSnapshot.getStorage().getPath();
+                            Log.d(TAG, "name: " + pictureName);
+                            Log.d(TAG, "url: " + url);
+                            showImage(url);
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    private void showImage(String url){
+        if(url != null && url.isEmpty() == false){
+            int width = Resources.getSystem().getDisplayMetrics().widthPixels;
+            Picasso.with(this)
+                    .load(url)
+                    .resize(width, width*2/3)
+                    .centerCrop()
+                    .into(profileImage);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        StorageReference ref = FirebaseStorage.getInstance().getReference().child(g + "/image");
+
+        try {
+            final File localFile = File.createTempFile("pic", "jpg");
+            ref.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    profileImage.setImageURI(Uri.fromFile(localFile));
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(Profile.this, "Unable to Get Profile Image", Toast.LENGTH_LONG).show();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
